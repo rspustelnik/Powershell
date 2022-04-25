78cf9020b6516c61087d670d231216db90c2476f
pusters <shawn.pustelnik@dillards.com>
2022-04-25 14:06:01 -0500

    meraki

---
+++ merakiapi.ps1
@@ -0,0 +1,9 @@
+﻿$cid="946058"
+$url_main="https://api.meraki.com/api/v0/organizations/$cid/deviceStatuses"
+$security_token=""
+$header = @{'X-Cisco-Meraki-API-Key'=$security_token}
+$apiDataRaw=Invoke-WebRequest -Headers $header   -Method Get   -Uri $url_main   
+$apiDataObj = ConvertFrom-Json $apiDataRaw.Content
+$offline = $apiDataObj|Where-Object{$_.status -eq "offline"}
+$offline.name
+$offline.count
