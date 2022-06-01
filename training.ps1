74a671ef35be03739f3e39ba755eb657eb9b81f1
Robert Pustelnik <rspustelnik@gmail.com>
2022-06-01 14:59:08 -0500

    June 1, 2022 2:59 PM

--- training.ps1
+++ training.ps1
@@ -1,4 +1,2 @@
-(Get-ADGroupMember vendors).samaccountname | % {
-    $vendorid = $_
-    if ((Get-ADuser $vendorid -Properties memberof).memberof -contains 'CN=GlobalProtectAllowed,OU=Groups,OU=DDSUsers,DC=dds,DC=dillards,DC=net') { $vendorid }
-}
+(Get-ADGroupMember vendors).samaccountname | ? { (Get-ADuser $_ -Properties memberof).memberof -contains 'CN=GlobalProtectAllowed,OU=Groups,OU=DDSUsers,DC=dds,DC=dillards,DC=net' }
+
