02f0f6e71442d83fe95861751e048186b8e9cae7
Robert Pustelnik <rspustelnik@gmail.com>
2022-06-01 13:25:16 -0500

    June 1, 2022 1:25 PM

--- training.ps1
+++ training.ps1
@@ -1,1 +1,8 @@
+$token = 'glpat-2ReyRiZF4YDuC6F2wASm'
+$header = @{
+    'PRIVATE-TOKEN' = $token
+}
+$uri = 'https://gitlab.dillards.com/api/v4/projects/'
+$projects = Invoke-WebRequest -Headers $header -Uri $uri
 
+$projects.Content
