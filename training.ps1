1c0e609c42cb94067a7d430332bb3630856449a7
Robert Pustelnik <rspustelnik@gmail.com>
2022-06-14 13:56:56 -0500

    June 14, 2022 1:56 PM

--- training.ps1
+++ training.ps1
@@ -1,2 +1,4 @@
-(Get-ADGroupMember vendors).samaccountname | ? { (get-ADuser $_ -Properties memberof).memberof -notcontains 'CN=GlobalProtectAllowed,OU=Groups,OU=DDSUsers,DC=dds,DC=dillards,DC=net' }
-
+$url_main = "https://gitlab.dillards.com/api/v4/users?state=active"
+$security_token = "glpat-2ReyRiZF4YDuC6F2wASm"
+$header = @{'PRIVATE-TOKEN' = $security_token }
+$apiDataRaw = Invoke-WebRequest -Headers $header   -Method Get   -Uri $url_main 
