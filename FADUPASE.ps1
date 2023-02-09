960f7c36533f0a8dd1de668ef54716f8201fea53
Robert Pustelnik <rspustelnik@gmail.com>
2023-02-09 17:08:00 +0000

    Feb 9, 2023, 11:08 AM

--- FADUPASE.ps1
+++ FADUPASE.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem *.pgp | foreach-object {
     if (get-childitem out.file) { Remove-Item out.file ; rename-item $_ out.file -Force } else { Rename-Item $_ out.file -Force } 
     Get-ChildItem *.pgp
-    get-childitem *.file
+    Get-childitem *.file
 }
